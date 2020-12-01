6f251408bacc59c2ce3a57aeadb40e665d7d156a
PedroAcevedo <pdacevedo@uninorte.edu.co>
2020-12-01 11:22:41 -0500

    Data visualizaction - air dataset

--- final_project/main.py
+++ final_project/main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import sys
 import argparse
+import netCDF4 as nc
 
 import vtk
 
@@ -50,66 +51,99 @@ class Visualization(object):
 	def __init__(self, args):
 		self.satellite = args.satellite
 		self.data = args.file
-		self.wrap = vtk.vtkWarpScalar()
-		self.TubeMapper = vtk.vtkTubeFilter()
+		self.wrap = vtk.vtkPolyDataMapper()
 
-		minWrap = 0
-		maxWrap = 100
-		minTube = -10000
-		maxTube = 8000
+		minWrap = 1
+		maxWrap = 5000
+		
+		self.ncDataSet = vtk.vtkNetCDFCFReader()
+		self.ncDataSet.SetFileName(self.data)
+		self.ncDataSet.UpdateMetaData()
+		self.ncDataSet.SetVariableArrayStatus('air', 1)
+		self.ncDataSet.Update()
 
-		bathymetryDataset = vtk.vtkXMLPolyDataReader()
-		bathymetryDataset.SetFileName(self.data)
+		assignAttribute = vtk.vtkAssignAttribute()
+		assignAttribute.SetInputConnection(self.ncDataSet.GetOutputPort())
+		assignAttribute.Assign("air", "SCALARS","CELL_DATA")
 
-		bathymetryDataset.Update()
-		imageDataset = vtk.vtkJPEGReader()
-		imageDataset.SetFileName(self.satellite)
-
-		#Filter geometry of the bathymetry dataset
-		self.wrap.SetInputConnection(bathymetryDataset.GetOutputPort())
-		self.wrap.SetScaleFactor(0.5)
-
-		# Create texture from satellite images
-		texture = vtk.vtkTexture()
-		texture.SetInputConnection(imageDataset.GetOutputPort())
-
-		# Wrapmapper is defined
-		WrapMapper = vtk.vtkDataSetMapper()
-		WrapMapper.SetInputConnection(self.wrap.GetOutputPort())
-		WrapMapper.SetScalarRange(minWrap, maxWrap)
-		WrapMapper.ScalarVisibilityOff()
-
-		# actor that combines mapper and texture for Wrap
-		actor = vtk.vtkActor()
-		actor.SetMapper(WrapMapper)
-		actor.SetTexture(texture)
+		surfaceFilter = vtk.vtkDataSetSurfaceFilter()
+		surfaceFilter.SetInputConnection(assignAttribute.GetOutputPort())
 
 		# Setup color mapping
-		lut = vtk.vtkColorTransferFunction()
-		lut.SetColorSpaceToRGB()
-		lut.AddRGBPoint(-10000, 0, 0.651, 1)
-		lut.AddRGBPoint(-1000, 0.91, 0.96, 1)
-		lut.AddRGBPoint(0, 1, 0, 0)
-		lut.AddRGBPoint(1000, 0.99, 1, 0.89)
-		lut.AddRGBPoint(8000, 1, 1, 0)
+		color_scale = vtk.vtkColorTransferFunction()
+		color_scale.SetColorSpaceToRGB()
+		color_scale.AddRGBPoint(180, 255/255, 228/255, 111/255) 			
+		color_scale.AddRGBPoint(250, 249/255, 168/255, 35/255) 			
+		color_scale.AddRGBPoint(350, 234/255, 42/255, 3/255)
 
-		#Defines the contour filter over wrap data
-		contour = vtk.vtkContourFilter()
-		contour.GenerateValues(19, minTube, maxTube)
-		contour.SetInputConnection(self.wrap.GetOutputPort())
 
-		# mapper is define
-		self.TubeMapper.SetInputConnection(contour.GetOutputPort())
-		self.TubeMapper.SetRadius(5000)
+		#Color Bar
+		color_bar = vtk.vtkScalarBarActor()
+		#color_bar.SetOrientationToHorizontal()
+		color_bar.SetLookupTable(color_scale)
+		color_bar.SetTitle("Temperature scale")
+		color_bar.SetLabelFormat("%4.0f")
+		color_bar.SetPosition(0.9, 0.1)
+		color_bar.SetWidth(0.1)
+		color_bar.SetHeight(0.7)
+
+		# #Defines the contour filter over wrap data
+		# contour = vtk.vtkContourFilter()
+		# contour.GenerateValues(19, minTube, maxTube)
+		# contour.SetInputConnection(self.wrap.GetOutputPort())
+
+		# # mapper is define
+		# self.TubeMapper.SetInputConnection(contour.GetOutputPort())
+		# self.TubeMapper.SetRadius(5000)
 
 		# Mapper for the height value is define
 		heightMapper = vtk.vtkDataSetMapper()
-		heightMapper.SetInputConnection(self.TubeMapper.GetOutputPort())
-		heightMapper.SetLookupTable(lut)
-
+		heightMapper.SetInputConnection(surfaceFilter.GetOutputPort())
+		heightMapper.SetLookupTable(color_scale)
+		#heightMapper.ScalarVisibilityOff()
+		#heightMapper.SetScalarRange(100,2500)
+		#heightMapper.SetScalarModeToUsePointFieldData()
+		
 		# Actor with colored Isocontours
 		coloredActor = vtk.vtkActor()
 		coloredActor.SetMapper(heightMapper)
+		coloredActor.GetProperty().SetOpacity(0.6) 
+		#coloredActor.SetTexture(texture)
+
+		source = vtk.vtkTexturedSphereSource()
+		source.SetRadius(1000)
+		source.SetThetaResolution(100)
+		source.SetPhiResolution(12)
+		imageDataset = vtk.vtkJPEGReader()
+		imageDataset.SetFileName(self.satellite)
+
+		texture = vtk.vtkTexture() 
+		texture.SetInputConnection(imageDataset.GetOutputPort()) 
+
+		transformTexture = vtk.vtkTransformTextureCoords() 
+		transformTexture.SetInputConnection(source.GetOutputPort()) 
+		#transformTexture.SetPosition(translate) 
+
+		mapper = vtk.vtkPolyDataMapper() 
+		mapper.SetInputConnection(transformTexture.GetOutputPort()) 
+		mapper.ScalarVisibilityOff()
+
+		actor = vtk.vtkActor() 
+		actor.SetMapper( mapper ) 
+		actor.SetTexture( texture ) 
+
+		# earthSource = vtk.vtkEarthSource()
+		# earthSource.SetRadius(1000)
+		# earthSource.OutlineOn()
+		# earthSource.Update()
+
+		# #Create a mapper and actor
+		# mapper = vtk.vtkPolyDataMapper()
+		# mapper.SetInputConnection(earthSource.GetOutputPort())
+
+		# actor = vtk.vtkActor()
+		# actor.SetMapper(mapper)
+
 
 		# Render
 		renderer_ = vtk.vtkRenderer()
@@ -119,13 +153,19 @@ class Visualization(object):
 		interactor.SetRenderWindow(render_window)
 
 		# Add slider for parameters modfication
-		SliderWidget = Slider(0.1,0.1,minWrap,maxWrap,'Wrap factor',self.wrap.SetScaleFactor).SetInteractor(interactor)
-		SliderWidget2 = Slider(0.1,0.2,0,(minTube*-10),'Isocontours factor', self.TubeMapper.SetRadius, value=5000).SetInteractor(interactor)
+		SliderWidget = Slider(0.1,0.1,minWrap,maxWrap,'Wrap factor',self.changeTime).SetInteractor(interactor)
+		#SliderWidget2 = Slider(0.1,0.2,0,(minTube*-10),'Isocontours factor', self.TubeMapper.SetRadius, value=5000).SetInteractor(interactor)
 		
 		# Add the actors to the renderer, define the background and size
 		renderer_.AddActor(coloredActor)
 		renderer_.AddActor(actor)
+		renderer_.AddActor(color_bar)
+
+		#renderer_.AddActor(actor)
 		renderer_.ResetCamera()
+		renderer_.SetUseDepthPeeling(1)
+		renderer_.SetMaximumNumberOfPeels(10)
+		renderer_.SetOcclusionRatio(0.4)
 		renderer_.SetBackground(0.1, 0.2, 0.4)
 
 		render_window.SetSize(800, 600)
@@ -136,6 +176,9 @@ class Visualization(object):
 		interactor.Start()
 
 
+	def changeTime(self,time):
+		self.ncDataSet.UpdateTimeStep(time*10**-6)
+		self.ncDataSet.Update()
 
 if __name__ == "__main__":
 	# --define argument parser and parse arguments--
